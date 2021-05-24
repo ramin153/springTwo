1b828d5728376e8fe0f514666577c4b6182fb3a2
ramin rowshan <raminrowshan153@gmail.com>
2021-05-24 12:38:34 +0430

    default profile

--- src/main/java/com/example/dependency_injection/service/I18NPersianService.java
+++ src/main/java/com/example/dependency_injection/service/I18NPersianService.java
@@ -3,7 +3,7 @@ package com.example.dependency_injection.service;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("FA")
+@Profile({"FA","default"})
 @Service("i18NService")
 public class I18NPersianService implements GreetingService {
     @Override
