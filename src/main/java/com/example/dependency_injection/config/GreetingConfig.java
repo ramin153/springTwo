9b1f4ac096c82f8924a0ba908b96c3686b04f144
ramin rowshan <raminrowshan153@gmail.com>
2021-05-30 20:16:30 +0430

    application properties

--- src/main/java/com/example/dependency_injection/config/GreetingConfig.java
+++ src/main/java/com/example/dependency_injection/config/GreetingConfig.java
@@ -9,7 +9,7 @@ import com.example.pet.PetServiceFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+
 @ImportResource("classpath:sfgdl-config.xml")
 @Configuration
 public class GreetingConfig {
